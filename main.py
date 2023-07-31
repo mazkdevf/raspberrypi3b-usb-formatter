3c6ec19d1b3694242d0ba2eff6a6f808a028c189
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2023-08-01 00:45:04 +0300

    FI

--- main.py
+++ main.py
@@ -3,58 +3,58 @@ import time
 import threading
 import subprocess
 
-def get_connected_usb_devices():
+def USBLaitteet():
     lsblk_cmd = "lsblk -o NAME,TYPE,RM | grep disk | awk '{print $1}'"
     output = subprocess.check_output(lsblk_cmd, shell=True, universal_newlines=True)
     usb_list = set(output.strip().split('\n'))
     return usb_list
 
-def unmount_usb(device):
+def MountUSB(laite):
     try:
-        unmount_cmd = f"udisksctl unmount -b /dev/{device}"
+        unmount_cmd = f"udisksctl unmount -b /dev/{laite}"
         subprocess.run(unmount_cmd, shell=True, check=True)
     except Exception as e:
-        print(f"Error unmounting USB device {device}: {e}")
+        print(f"Virhe irrottaessa USB-laitetta {laite}: {e}")
 
-def format_usb(device):
+def FormatoiUSB(laite):
     try:
-        format_cmd = f"sudo parted /dev/{device} mktable msdos mkpart primary fat32 1MiB 100% set 1 boot on"
+        format_cmd = f"sudo parted /dev/{laite} mktable msdos mkpart primary fat32 1MiB 100% set 1 boot on"
         subprocess.run(format_cmd, shell=True, check=True)
-        format_fs_cmd = f"sudo mkfs.vfat -n 'USB' /dev/{device}"
+        format_fs_cmd = f"sudo mkfs.vfat -n 'USB' /dev/{laite}"
         subprocess.run(format_fs_cmd, shell=True, check=True)
-        print(f"Formatting USB device: {device}")
+        print(f"USB-laitteen alustus: {laite}")
     except Exception as e:
-        print(f"Error formatting USB device {device}: {e}")
+        print(f"Virhe alustettaessa USB-laitetta {laite}: {e}")
 
-def on_format_button_click(device):
-    user_response = input(f"Do you want to format {device}? (yes/no): ").strip().lower()
-    if user_response == "yes":
-        unmount_usb(device)
-        format_usb(device)
+def UserInput(laite):
+    user_response = input(f"Haluatko alustaa laitteen {laite}? (kyllä/ei): ").strip().lower()
+    if user_response == "kyllä":
+        MountUSB(laite)
+        FormatoiUSB(laite)
     else:
-        print("Formatting process canceled.")
+        print("Alustusprosessi peruttu.")
 
-def monitor_usb():
-    connected_usb_devices = get_connected_usb_devices()
+def USBLooppi():
+    liitetyt_usb_laitteet = USBLaitteet()
 
     while True:
-        new_usb_devices = get_connected_usb_devices()
+        uudet_usb_laitteet = USBLaitteet()
 
-        for device in new_usb_devices - connected_usb_devices:
-            print(f"USB device inserted: {device}")
+        for laite in uudet_usb_laitteet - liitetyt_usb_laitteet:
+            print(f"USB-laite liitetty: {laite}")
 
-            on_format_button_click(device)
+            UserInput(laite)
 
-        for device in connected_usb_devices - new_usb_devices:
-            print(f"USB device disconnected: {device}")
+        for laite in liitetyt_usb_laitteet - uudet_usb_laitteet:
+            print(f"USB-laite irrotettu: {laite}")
 
-        connected_usb_devices = new_usb_devices
+        liitetyt_usb_laitteet = uudet_usb_laitteet
         time.sleep(2)
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
-        print("This script needs to be runned with SUDO.")
+        print("Tämä skripti tulee ajaa SUDO-oikeuksilla.")
         exit(1)
 
-    print("Raspberry PI 3 B USB Formattor :D")
-    monitor_usb()
+    print("Raspberry PI 3B USB Alustaja :D")
+    USBLooppi()
